9c570e2630d12f61184be9874615c362c09e5002
Kyle Shannon <kyle@pobox.com>
2018-04-30 16:18:56 -0600

    fix package

--- io.go
+++ io.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main
+package misc
 
 import (
 	"io/ioutil"
@@ -11,7 +11,7 @@ import (
 
 // ReadOrPanic reads an entire file and returns a string trimmed of whitespace.
 // Any error encountered is considered fatal.
-func ReadOrPanic(path string) {
+func ReadOrPanic(path string) string {
 	buf, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err.Error())
